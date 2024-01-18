406483a69580a6691b91d62fa185095f7d9221d5
kausik10 <kausikpaudel@gmail.com>
2024-01-18 11:36:58 +0545

    Added Page routing and structure of folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import "./App.css";
+import React from "react";
+import { sampleProducts } from "./data";
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl underline ">HELLO KE-commerce</h1>
+      <header>
+        <h1 className="text-4xl font-bold py-4"> KE-commerce</h1>
+      </header>
+      <main>
+        <ul className="flex flex-col justify-center items-center gap-5">
+          {sampleProducts.map((product, index) => (
+            <li key={index}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="max-w-[400px] w-full"
+              />
+              <h2 className="text-2xl underline">{product.name}</h2>
+              <p className="text-xl">${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer>KE-commerce. All rights reserved</footer>
     </>
   );
 }
