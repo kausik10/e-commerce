6e1d191f8eeae625cda2e893b8bb520aeef6ba32
kausik10 <kausikpaudel@gmail.com>
2024-01-22 11:02:00 +0545

    Added Helmet to rename the pages on render

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Product Page</title>
+      </Helmet>
+      <h1>Product Page</h1>
+    </div>
+  );
 };
 
 export default ProductPage;
