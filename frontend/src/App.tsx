3f1379832b156e7a40434098be78a6fac08ffa6c
kausik10 <kausikpaudel@gmail.com>
2024-03-09 23:14:35 +0545

    Feature Cart Page Implementation. Buggy <AlertDialog>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,19 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
-import { StoreProvider } from "./Store";
 import { Toaster } from "./component/ui/toaster";
 function App() {
   return (
-    <StoreProvider>
-      <div className="w-full flex flex-col">
-        <Navbar />
+    <div className="w-full flex flex-col">
+      <Navbar />
 
-        <main className="my-2 mx-auto w-4/5 flex flex-1 justify-center items-start">
-          <Outlet />
-        </main>
+      <main className="my-2 mx-auto w-4/5 flex flex-1 justify-center items-start">
+        <Outlet />
+      </main>
 
-        <Toaster />
-        <Footer />
-      </div>
-    </StoreProvider>
+      <Toaster />
+      <Footer />
+    </div>
   );
 }
 
