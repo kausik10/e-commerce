730b8756d9b9e597304ae15aa0c3dae5d4be296f
kausik10 <kausikpaudel@gmail.com>
2024-08-07 14:32:17 +0545

    Integrated Paypal for Paying Order

--- backend/src/routers/orderRouter.ts
+++ backend/src/routers/orderRouter.ts
@@ -47,3 +47,28 @@ orderRouter.post(
     }
   }),
 );
+
+
+orderRouter.put(
+  ':id/pay',
+  isAuth,
+  expressAsyncHandler(async (req: Request, res: Response) => {
+    const order = await OrderModel.findById(req.params.id).populate('user');
+
+    if(order)
+    {
+      order.isPaid = true;
+      order.paidAt = new Date(Date.now());
+      order.paymentResult = {
+        paymentId: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.payer.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.send({order: updatedOrder, message: 'Order Paid Succesfully'});
+    }else {
+      res.status(404).send({message: 'Order Not Found'})
+    }
+})
+);
--- frontend/src/pages/OrderPage.tsx
+++ frontend/src/pages/OrderPage.tsx
@@ -1,3 +1,4 @@
+
 import { Store } from "@/Store";
 import { Alert } from "@/component/ui/alert";
 import {
@@ -7,21 +8,111 @@ import {
   CardFooter,
   CardContent,
 } from "@/component/ui/card";
+
+import {
+  PayPalButtons,
+  PayPalButtonsComponentProps,
+  SCRIPT_LOADING_STATE,
+  usePayPalScriptReducer,
+} from '@paypal/react-paypal-js';
+
 import { toast } from "@/component/ui/use-toast";
 import LoadingBox from "@/components/LoadingBox";
-import { userOrderDetailQuery } from "@/hooks/orderHooks";
+import { useGetPaypalClientIdQuery, usePayOrderMutation, userOrderDetailQuery } from "@/hooks/orderHooks";
 import { ApiError } from "@/type/ApiErros";
 import { getError } from "@/utils";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams, Link } from "react-router-dom";
+import { Button } from "@/component/ui/button";
 
 export default function OrderPage() {
   const { state } = useContext(Store);
   const { userInfo } = state;
+
   const params = useParams();
   const { id: orderId } = params;
-  const { data: order, isLoading, error } = userOrderDetailQuery(orderId!);
+
+  const { data: order, isLoading, error, refetch } = userOrderDetailQuery(orderId!);
+
+  const {mutateAsync: payOrder, isLoading: loadingPayment} = usePayOrderMutation();
+
+  // for development mode only 
+  const testPayHandler = async() => {
+    await payOrder({orderId: orderId!});
+    refetch();
+    toast({
+      title: "Success",
+      description: "Payment Successful",
+    });
+  }
+
+const [{isPending, isRejected}, paypalDispatch] = usePayPalScriptReducer();
+
+const {data: paypalConfig} = useGetPaypalClientIdQuery();
+
+useEffect(() => {
+  if (paypalConfig && paypalConfig.clientId) {
+    const loadPaypalScript = async () => {
+      paypalDispatch({
+        type: 'resetOptions',
+        value: {
+          'client-id': paypalConfig!.clientId,
+          currency: 'USD',
+        },
+      })
+      paypalDispatch({
+        type: 'setLoadingStatus',
+        value: SCRIPT_LOADING_STATE.PENDING,
+      })
+    }
+    loadPaypalScript()
+  }
+}, [paypalConfig])
+
+const paypalbuttonTransactionProps: PayPalButtonsComponentProps = {
+  style: { layout: 'vertical' },
+  createOrder(data, actions) {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: {
+              value: order!.totalPrice.toString(),
+            },
+          },
+        ],
+      })
+      .then((orderID: string) => {
+        return orderID
+      })
+  },
+  onApprove(data, actions) {
+    return actions.order!.capture().then(async (details) => {
+      try {
+        await payOrder({ orderId: orderId!, ...details })
+        refetch()
+        toast({
+          title: "Success",
+          description: "Payment Successful",
+        });
+      } catch (err) {
+        toast({
+          title: "Failed",
+          variant: "destructive",
+          description: getError(err as ApiError),
+        });
+      }
+    })
+  },
+  onError: (err: Error) => {
+    toast({
+      title: "Failed",
+      variant: "destructive",
+      description: getError(err as ApiError),
+    });
+  },
+}
 
   return isLoading ? (
     <LoadingBox />
@@ -38,13 +129,14 @@ export default function OrderPage() {
       description: "Order not found",
     })
   ) : (
+    <>
     <div>
       <Helmet>
         <title>Order {orderId}</title>
       </Helmet>
       <h1 className="my-3">Order {orderId}</h1>
       <div className="flex flex-row gap-5">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col justify-between gap-3">
           <Card className="mb-3 mt-3">
             <CardHeader>
               <CardTitle>Shipping</CardTitle>
@@ -157,10 +249,34 @@ export default function OrderPage() {
                   ${order.totalPrice.toFixed(2)}
                 </div>
               </div>
+              <div className="grid grid-cols-[1fr, 1fr] gap-3">
+                {!order.isPaid && (
+                  <div className="flex flex-col">
+                    {isPending ? (
+                      <LoadingBox />
+                    ) : isRejected ? (
+                      <Alert variant="destructive">
+                        "Paypal script loading failed"
+                      </Alert>
+                    ) : (
+                      <div>
+                      <PayPalButtons
+                        {...paypalbuttonTransactionProps}
+                      
+                      ></PayPalButtons>
+                      <Button onClick={testPayHandler}>Test Pay</Button>
+                      </div>
+                    )}
+                  </div>
+                )}
+                {loadingPayment && <LoadingBox />}
+
+              </div>
             </CardContent>
           </Card>
         </div>
       </div>
     </div>
+    </>
   );
 }
